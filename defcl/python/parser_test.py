b6816ea1d6056b107a2d626bd9308edd35d054d6
Max Kanat-Alexander <avatraxiom@gmail.com>
2026-02-04 04:51:07 -0700

    Make the DCL parser_test more explicit about what error it expects.

--- defcl/python/parser_test.py
+++ defcl/python/parser_test.py
@@ -143,191 +143,301 @@ class TestValidFiles:
 
 class TestInvalidBooleans:
     def test_false_literal(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "booleans" / "false_literal.defcl"
             )
+        assert exc_info.value.token.type == "FIELD_NAME"
+        assert exc_info.value.token.value == "false"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 14
 
     def test_true_literal(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "booleans" / "true_literal.defcl")
+        assert exc_info.value.token.type == "FIELD_NAME"
+        assert exc_info.value.token.value == "true"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 14
 
 
 class TestInvalidEnums:
     def test_lowercase_enum(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "enums" / "lowercase_enum.defcl")
+        assert exc_info.value.token.type == "FIELD_NAME"
+        assert exc_info.value.token.value == "active"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 13
 
     def test_mixed_case_enum(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "enums" / "mixed_case_enum.defcl")
+        assert exc_info.value.token.type == "RBRACE"
+        assert exc_info.value.line == 3
+        assert exc_info.value.column == 1
 
 
 class TestInvalidFieldNames:
     def test_double_underscore(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "field_names" / "double_underscore.defcl"
             )
+        assert exc_info.value.char == "_"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 11
 
     def test_hyphen(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "field_names" / "hyphen.defcl")
+        assert exc_info.value.char == "-"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 13
 
     def test_leading_digit(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "field_names" / "leading_digit.defcl"
             )
+        assert exc_info.value.token.type == "INTEGER"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 5
 
     def test_leading_underscore(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "field_names" / "leading_underscore.defcl"
             )
+        assert exc_info.value.char == "_"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 5
 
     def test_period(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "field_names" / "period.defcl")
+        assert exc_info.value.char == "."
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 13
 
     def test_trailing_underscore(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "field_names" / "trailing_underscore.defcl"
             )
+        assert exc_info.value.char == "_"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 18
 
     def test_underscore_digit(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "field_names" / "underscore_digit.defcl"
             )
+        assert exc_info.value.char == "_"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 11
 
     def test_uppercase(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "field_names" / "uppercase.defcl")
+        assert exc_info.value.token.type == "ENUM_VALUE"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 5
 
 
 class TestInvalidFileFormat:
     def test_bom(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "file_format" / "bom.defcl")
+        assert exc_info.value.char == "\ufeff"
+        assert exc_info.value.line == 1
+        assert exc_info.value.column == 1
 
 
 class TestInvalidMessages:
     def test_angle_brackets(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "messages" / "angle_brackets.defcl"
             )
+        assert exc_info.value.char == "<"
+        assert exc_info.value.line == 1
+        assert exc_info.value.column == 10
 
 
 class TestInvalidNumbers:
     def test_hex(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "numbers" / "hex.defcl")
+        assert exc_info.value.token.type == "ENUM_VALUE"
+        assert exc_info.value.token.value == "A"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 15
 
     def test_octal(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "numbers" / "octal.defcl")
+        assert exc_info.value.token.type == "INTEGER"
+        assert exc_info.value.token.value == "777"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 13
 
     def test_scientific_int(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "numbers" / "scientific_int.defcl"
             )
+        assert exc_info.value.token.type == "RBRACE"
+        assert exc_info.value.line == 3
+        assert exc_info.value.column == 1
 
     def test_scientific_float(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "numbers" / "scientific_float.defcl"
             )
+        assert exc_info.value.token.type == "INTEGER"
+        assert exc_info.value.token.value == "-2"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 16
 
     def test_leading_zeros(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "numbers" / "leading_zeros.defcl")
+        assert exc_info.value.token.type == "INTEGER"
+        assert exc_info.value.token.value == "0"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 13
 
     def test_leading_decimal(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "numbers" / "leading_decimal.defcl"
             )
+        assert exc_info.value.char == "."
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 12
 
     def test_trailing_decimal(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "numbers" / "trailing_decimal.defcl"
             )
+        assert exc_info.value.char == "."
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 14
 
     def test_plus_sign(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "numbers" / "plus_sign.defcl")
+        assert exc_info.value.char == "+"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 12
 
     def test_space_after_sign(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "numbers" / "space_after_sign.defcl"
             )
+        assert exc_info.value.char == "-"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 12
 
     def test_type_suffix_f(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "numbers" / "type_suffix_f.defcl")
+        assert exc_info.value.token.type == "RBRACE"
+        assert exc_info.value.line == 3
+        assert exc_info.value.column == 1
 
     def test_type_suffix_upper_f(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "numbers" / "type_suffix_upper_f.defcl"
             )
+        assert exc_info.value.token.type == "ENUM_VALUE"
+        assert exc_info.value.token.value == "F"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 14
 
 
 class TestInvalidSeparators:
     def test_comma_separator(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "separators" / "comma_separator.defcl"
             )
+        assert exc_info.value.token.type == "COMMA"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 9
 
     def test_missing_colon(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "separators" / "missing_colon.defcl"
             )
+        assert exc_info.value.token.type == "STRING"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 11
 
     def test_semicolon(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "separators" / "semicolon.defcl")
+        assert exc_info.value.char == ";"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 18
 
 
 class TestInvalidStrings:
     def test_raw_newline(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "strings" / "raw_newline.defcl")
+        assert exc_info.value.char == '"'
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 12
 
     def test_single_quotes(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "strings" / "single_quotes.defcl")
+        assert exc_info.value.char == "'"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 12
 
 
 class TestInvalidToplevel:
     def test_missing_toplevel_colon(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "toplevel" / "missing_toplevel_colon.defcl"
             )
+        assert exc_info.value.token.type == "LBRACE"
+        assert exc_info.value.line == 1
+        assert exc_info.value.column == 9
 
     def test_scalar_toplevel(self):
-        with pytest.raises(lark.exceptions.UnexpectedToken):
+        with pytest.raises(lark.exceptions.UnexpectedToken) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "toplevel" / "scalar_toplevel.defcl"
             )
+        assert exc_info.value.token.type == "STRING"
+        assert exc_info.value.line == 1
+        assert exc_info.value.column == 7
 
 
 class TestInvalidWhitespace:
     def test_carriage_return(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(
                 _INVALID_PARSER_PATH / "whitespace" / "carriage_return.defcl"
             )
+        assert exc_info.value.char == "\r"
+        assert exc_info.value.line == 1
+        assert exc_info.value.column == 10
 
     def test_tab_char(self):
-        with pytest.raises(lark.exceptions.UnexpectedCharacters):
+        with pytest.raises(lark.exceptions.UnexpectedCharacters) as exc_info:
             _parser.parse_file(_INVALID_PARSER_PATH / "whitespace" / "tab_char.defcl")
+        assert exc_info.value.char == "\t"
+        assert exc_info.value.line == 2
+        assert exc_info.value.column == 1
