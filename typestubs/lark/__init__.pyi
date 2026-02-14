1f9fbe38e6db4e419abb4b76245bc4d83e680dd6
Max Kanat-Alexander <avatraxiom@gmail.com>
2026-02-14 08:23:56 -0500

    ruff format the typestubs

--- typestubs/lark/__init__.pyi
+++ typestubs/lark/__init__.pyi
@@ -3,10 +3,17 @@ from lark import tree as tree
 from lark import visitors as visitors
 from lark.lark import Lark as Lark
 from lark.lexer import Token as Token
-from lark.tree import ParseTree as ParseTree, Tree as Tree
+from lark.tree import ParseTree as ParseTree
+from lark.tree import Tree as Tree
 from lark.visitors import (
     Discard as Discard,
+)
+from lark.visitors import (
     Transformer as Transformer,
+)
+from lark.visitors import (
     Visitor as Visitor,
+)
+from lark.visitors import (
     v_args as v_args,
 )
