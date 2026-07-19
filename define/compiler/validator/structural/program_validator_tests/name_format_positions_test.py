72cd30be76dce36f96a7b4d0aa3a879ddda4be20
Max Kanat-Alexander <avatraxiom@gmail.com>
2026-07-19 02:02:15 -0400

    Add name formatting tests that were previously only covered by the driver integration tests.

--- define/compiler/validator/structural/program_validator_tests/name_format_positions_test.py
+++ define/compiler/validator/structural/program_validator_tests/name_format_positions_test.py
@@ -25,6 +25,20 @@ def test_multiverse_name_position():
     assert diags[0].location.end_column == 34
 
 
+def test_multiverse_name_too_short():
+    source = "define the potential position<m:my.domain.com:my_lib:/path>.\n"
+    results = (
+        program_validator.ProgramStructuralValidator()
+        .validate_program_non_filesystem(source)
+        .file_results
+    )
+    diags = results[0].diagnostics
+    assert len(diags) == 2
+    assert isinstance(diags[0], diagnostics.MultiverseNameTooShortDiagnostic)
+    assert diags[0].multiverse_name == "m"
+    assert isinstance(diags[1], diagnostics.ReservedMultiverseNameDiagnostic)
+
+
 def test_authority_domain_position():
     source = "define the potential position<mv:-example.com:my_lib:/path>.\n"
     results = (
@@ -41,6 +55,20 @@ def test_authority_domain_position():
     assert diags[0].location.column == 34
     assert diags[0].location.end_line == 1
     assert diags[0].location.end_column == 46
+
+
+def test_authority_domain_too_short():
+    source = "define the potential position<mv:a:my_lib:/path>.\n"
+    results = (
+        program_validator.ProgramStructuralValidator()
+        .validate_program_non_filesystem(source)
+        .file_results
+    )
+    diags = results[0].diagnostics
+    assert len(diags) == 2
+    assert isinstance(diags[0], diagnostics.AuthorityDomainTooShortDiagnostic)
+    assert diags[0].domain == "a"
+    assert isinstance(diags[1], diagnostics.DotlessAuthorityDomainDiagnostic)
 
 
 def test_authority_path_position():
@@ -61,6 +89,19 @@ def test_authority_path_position():
     assert diags[0].location.end_column == 55
 
 
+def test_authority_path_empty_segment():
+    source = "define the potential position<mv:my.domain.com//team:my_lib:/path>.\n"
+    results = (
+        program_validator.ProgramStructuralValidator()
+        .validate_program_non_filesystem(source)
+        .file_results
+    )
+    diags = results[0].diagnostics
+    assert len(diags) == 1
+    assert isinstance(diags[0], diagnostics.AuthorityPathEmptySegmentDiagnostic)
+    assert diags[0].authority == "my.domain.com//team"
+
+
 def test_universe_name_position():
     source = "define the potential position<mv:my.domain.com:_my_lib:/path>.\n"
     results = (
@@ -79,6 +120,19 @@ def test_universe_name_position():
     assert diags[0].location.end_column == 55
 
 
+def test_universe_name_too_short():
+    source = "define the potential position<mv:my.domain.com:t:/path>.\n"
+    results = (
+        program_validator.ProgramStructuralValidator()
+        .validate_program_non_filesystem(source)
+        .file_results
+    )
+    diags = results[0].diagnostics
+    assert len(diags) == 1
+    assert isinstance(diags[0], diagnostics.UniverseNameTooShortDiagnostic)
+    assert diags[0].universe_name == "t"
+
+
 def test_path_segment_position():
     source = "define the potential position<my.domain.com:my_lib:/2bad>.\n"
     results = (
@@ -95,6 +149,19 @@ def test_path_segment_position():
     assert diags[0].location.column == 53
     assert diags[0].location.end_line == 1
     assert diags[0].location.end_column == 57
+
+
+def test_global_name_path_empty_segment():
+    source = "define the potential position<mv:my.domain.com:my_lib:/foo//bar>.\n"
+    results = (
+        program_validator.ProgramStructuralValidator()
+        .validate_program_non_filesystem(source)
+        .file_results
+    )
+    diags = results[0].diagnostics
+    assert len(diags) == 1
+    assert isinstance(diags[0], diagnostics.GlobalNamePathEmptySegmentDiagnostic)
+    assert diags[0].path == "/foo//bar"
 
 
 def test_local_name_position():
